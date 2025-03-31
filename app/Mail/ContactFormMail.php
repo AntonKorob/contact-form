77553cdd2076304af8ef3312c57cb14571329284
AntonKorob <gorizont102.k@mail.ru>
2025-03-31 20:56:57 +0700

    create ContactFormMail

---
+++ app/Mail/ContactFormMail.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use App\Model\Contact;
+
+class ContactFormMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $contact;
+    public function __construct(Contact $contact)
+    {
+        $this->contact = $contact;
+    }
+
+    public function build()
+    {
+        return $this->subjrct('New application from contact form')
+                    ->view('emails.contact-form');
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Contact Form Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'view.name',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
